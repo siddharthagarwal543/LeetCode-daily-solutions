32b069b01de98f515af82162253369c7ee0ed38e
siddharthagarwal543 <78862240+siddharthagarwal543@users.noreply.github.com>
2023-03-18 00:00:35 +0530

    Create Implement Trie (Prefix Tree) 17 March 2023.cpp

---
+++ Implement Trie (Prefix Tree) 17 March 2023.cpp
@@ -0,0 +1,79 @@
+class trieNode{
+    public:
+    char data;
+    trieNode* children[58];
+    bool isTerminal;
+    trieNode(char ch){
+        data=ch;
+        for(int i=0;i<58;i++)
+        children[i]=NULL;
+        isTerminal=false;
+    }
+};
+
+class Trie {
+public:
+    trieNode* root;
+    Trie() {
+        root=new trieNode('\0'); 
+    }
+    void insertUtil(trieNode* root,string word)
+    {
+        if(word.length()==0)
+        {
+            root->isTerminal=true;
+            return;
+        }
+        int index=word[0]-'A';
+        trieNode* child;
+        if(root->children[index]!=NULL)
+        child=root->children[index];
+        else
+        {
+            child=new trieNode(word[0]);
+            root->children[index]=child;
+        }
+        insertUtil(child,word.substr(1));
+    }
+    void insert(string word) {
+     insertUtil(root,word);   
+    }
+    bool searchUtil(trieNode* root, string word){
+        if(word.length()==0)
+        return root->isTerminal;
+        int index=word[0]-'A';
+    trieNode* child;
+
+    if(root->children[index]!=NULL)
+    child=root->children[index];
+    else
+    return false;
+    return searchUtil(child,word.substr(1));
+    }
+    bool search(string word) {
+     return searchUtil(root,word);   
+    }
+    bool prefixUtil(trieNode* root, string word){
+        if(word.length()==0)
+        return true;
+        int index=word[0]-'A';
+    trieNode* child;
+
+    if(root->children[index]!=NULL)
+    child=root->children[index];
+    else
+    return false;
+    return prefixUtil(child,word.substr(1));
+    }
+    bool startsWith(string prefix) {
+        return prefixUtil(root,prefix);
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
